c878e151c350729fcffff6f31c0d91bbd4b5aa86
Laura DeCicco <ldecicco@usgs.gov>
2012-08-23 17:09:24 -0500

    Updated

--- R/loadestPrintEval.R
+++ R/loadestPrintEval.R
@@ -15,39 +15,45 @@ loadestPrintEval <- function(evaluat, method) {
   ## report the results for AMLE
   if(method == "AMLE") {
     cat("\n Adjusted Maximum Likelihood Estimate of the selected model.\n")
-    if(evaluat$IERR < 0)
-      cat("\n\n**** Warning excessive censoring, code: ", evaluat$IERR,
-          "\n\n", sep="")
-    params <- evaluat$parms[1:evaluat$NPAR]
-    parmsd <- evaluat$stddev[1:evaluat$NPAR]
-    parmpv <- evaluat$pval[1:evaluat$NPAR]
+    
+    if(evaluat$IERR < 0) cat("\n\n**** Warning excessive censoring, code: ", evaluat$IERR,"\n\n", sep="")
+    
+    params <- evaluat$PARAML[1:evaluat$NPAR]
+    parmsd <- evaluat$STDDEV[1:evaluat$NPAR]
+    parmpv <- evaluat$PVAL[1:evaluat$NPAR]
     params <- cbind(params, parmsd, parmpv)
-    dimnames(params) <- list(names.params, c("Coefficient", "Std. Dev.", "P-value"))
+    
+    dimnames(params) <- list(NULL, c("Coefficient", "Std. Dev.", "P-value"))
+#     dimnames(params) <- list(names.params, c("Coefficient", "Std. Dev.", "P-value"))
+    
     print(params)
-    cat("\nEstimated residual variance = ", signif(evaluat$parms[evaluat$NPAR + 1], 4), "\n", sep="")
-    cat("R-squared of the regression =", signif(evaluat$rsq, 4), "\n")
-    cat("Serial correlation of the residuals =", signif(evaluat$scorr, 4), "\n")
-    cat("\n Covariance/correlation of regression coefficients\n")
+    cat("\nEstimated residual variance = ", signif(evaluat$PARAML[evaluat$NPAR + 1], 4), "\n", sep="")
+    cat("R-squared of the regression =", signif(evaluat$RSQ, 4), "\n")
+    cat("Serial correlation of the residuals =", signif(evaluat$SCORR, 4), "\n")
+    cat("\n COVariance/correlation of regression coefficients\n")
     names.params <- c(names.params,"Scale")
-    dimnames(evaluat$cov) <- list(names.params,names.params)
-    std <- sqrt(diag(evaluat$cov))
-    evaluat$cov[lower.tri(evaluat$cov)] <- evaluat$cov[lower.tri(evaluat$cov)] /
-      outer(std,std)[lower.tri(evaluat$cov)]
-    print(evaluat$cov)
+    
+#     dimnames(evaluat$COV) <- list(names.params,names.params)
+    
+    std <- sqrt(diag(evaluat$COV))
+    evaluat$COV[lower.tri(evaluat$COV)] <- evaluat$COV[lower.tri(evaluat$COV)] /
+      outer(std,std)[lower.tri(evaluat$COV)]
+    print(evaluat$COV)
     cat("\n* NOTE: the following tests are merely informative, the user must evaluate the model more fully\n")
     cat("\nTurnbull-Weiss normality test statistic = ",
-        signif(evaluat$llraml, 4), ", P-value = ", signif(evaluat$plevaml, 4),
-        ", DF = ", signif(evaluat$df, 4), "\n", sep="")
-    Crit <- round(loadestGrubbsBeck(length(evaluat$resid)), 3)
+        signif(evaluat$LLRAML, 4), ", P-value = ", signif(evaluat$PLEVAML, 4),
+        ", DF = ", signif(evaluat$DF, 4), "\n", sep="")
+    Crit <- round(loadestGrubbsBeck(length(evaluat$RESID)), 3)
     cat("\nGrubbs & Beck 2-sided outlier test criteria at 5% significance\n")
     cat(-Crit, Crit, "\nRange of normalized residuals\n", sep=" ")
-    evaluat$scale <- sqrt(evaluat$parms[evaluat$NPAR + 1])
-    Range <- range(evaluat$resid) / evaluat$scale
+    evaluat$SCALE <- sqrt(evaluat$PARAML[evaluat$NPAR + 1])
+    Range <- range(evaluat$RESID) / evaluat$SCALE
     cat(round(Range,3), "\n", sep=" ")
-    if(max(abs(Range)) > Crit)
-      evaluat$outlier <- T
-    else
-      evaluat$outlier <- F
+    if(max(abs(Range)) > Crit){
+      evaluat$outlier <- TRUE
+    } else {
+      evaluat$outlier <- FALSE
+    }      
     return(evaluat)
   }
   ## report results for MLE
@@ -56,15 +62,15 @@ loadestPrintEval <- function(evaluat, method) {
     if(evaluat$IERR < 0)
       cat("\n\n**** Warning excessive censoring, code: ", evaluat$IERR,
           "\n\n", sep="")
-    params <- evaluat$parms[1:evaluat$NPAR]
+    params <- evaluat$PARMLE[1:evaluat$NPAR]
     names(params) <- names.params
     print(params)
     cat("\nEstimated residual variance = ",
-        signif(evaluat$parms[evaluat$NPAR + 1], 4), "\n", sep="")
+        signif(evaluat$PARMLE[evaluat$NPAR + 1], 4), "\n", sep="")
     cat("\n* NOTE: the following test is merely informative, the user must evaluate the model more fully\n")
     cat("\nTurnbull-Weiss normality test statistic = ",
-        signif(evaluat$llraml, 4), ", P-value = ", signif(evaluat$plevaml, 4),
-        ", DF = ", signif(evaluat$df, 4), "\n", sep="")
+        signif(evaluat$LLRAML, 4), ", P-value = ", signif(evaluat$PLEVAML, 4),
+        ", DF = ", signif(evaluat$DF, 4), "\n", sep="")
     return(evaluat)
   }
   ## report results for LAD, the last remaining option
@@ -72,7 +78,7 @@ loadestPrintEval <- function(evaluat, method) {
   params <- evaluat$parms[1:evaluat$NPAR]
   names(params) <- names.params
   print(params)
-  cat("\nSerial correlation of the residuals =",signif( evaluat$scorr, 4), "\n")
-  cat("Bias correction factor = ", signif(evaluat$bicor, 4), "\n", sep="")
+  cat("\nSerial correlation of the residuals =",signif( evaluat$SCORR, 4), "\n")
+  cat("Bias correction factor = ", signif(evaluat$BIAS, 4), "\n", sep="")
   invisible(evaluat)
 }
