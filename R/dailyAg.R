c7bbcf47556cdbcc1f74a1463906d7cf0dc3d0ac
Dave Lorenz <lorenz@usgs.gov>
2013-12-19 09:59:16 -0600

    bug fix in documentation for dailyAg

--- R/dailyAg.R
+++ R/dailyAg.R
@@ -3,11 +3,11 @@
 #' Compute daily mean water-quality values for data frames containing 
 #'water-quality data.
 #'
-#' @param x a data frame contining water-quality data.
+#' @param x a data frame containing water-quality data.
 #' @param dates the name of the sample date column.
 #' @param times the name of the sample time column.
 #'
-#' @return A data frame like \code{x] but with the means for each 
+#' @return A data frame like \code{x} but with the means for each 
 #'column by day and the sample time set to "1200."
 #'
 #' @export
